7efc981e2c833cf621eede896bfca753e1fa6ddf
tianchen2019 <18821690169@163.com>
2019-08-25 17:51:02 +0800

    Update ID_simbol_genename.r

--- ID_simbol_genename.r
+++ ID_simbol_genename.r
@@ -9,3 +9,4 @@ mouse = useMart("ensembl", dataset = "mmusculus_gene_ensembl")
 gene_up_id = getLDS(attributes = c("ensembl_gene_id"), filters = "ensembl_gene_id", 
                     values = gene_kras_up_filter$ensembl_gene_id , mart = mouse, attributesL = c("ensembl_gene_id", "hgnc_symbol", "entrezgene_id"), 
                     martL = human, uniqueRows=T)
+`https://www.jianshu.com/p/33e22a558457`
