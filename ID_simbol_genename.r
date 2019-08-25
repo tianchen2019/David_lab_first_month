1df7cd40f469ad49355a9f8b8b80975ebaae09fb
tianchen2019 <18821690169@163.com>
2019-08-25 17:50:07 +0800

    Create ID_simbol_genename.r

---
+++ ID_simbol_genename.r
@@ -0,0 +1,11 @@
+*Script : ID_simbol_genename.r*
+also can see in the network: https://www.jianshu.com/p/33e22a558457
+library(biomaRt)
+library(clusterProfiler)
+library(org.Hs.eg.db)
+human = useMart("ensembl", dataset = "hsapiens_gene_ensembl")
+mouse = useMart("ensembl", dataset = "mmusculus_gene_ensembl")
+
+gene_up_id = getLDS(attributes = c("ensembl_gene_id"), filters = "ensembl_gene_id", 
+                    values = gene_kras_up_filter$ensembl_gene_id , mart = mouse, attributesL = c("ensembl_gene_id", "hgnc_symbol", "entrezgene_id"), 
+                    martL = human, uniqueRows=T)
