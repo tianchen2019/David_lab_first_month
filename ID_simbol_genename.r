9144546acccce5552922c2fcee4249abf77086f3
tianchen2019 <18821690169@163.com>
2020-01-10 19:57:21 +0800

    Update ID_simbol_genename.r

--- ID_simbol_genename.r
+++ ID_simbol_genename.r
@@ -1,8 +1,6 @@
 *Script : ID_simbol_genename.r*
 also can see in the network: https://www.jianshu.com/p/33e22a558457     ； https://www.jianshu.com/p/f7d1f0ffb27e
 library(biomaRt)
-library(clusterProfiler)
-library(org.Hs.eg.db)
 human = useMart("ensembl", dataset = "hsapiens_gene_ensembl")
 mouse = useMart("ensembl", dataset = "mmusculus_gene_ensembl")
 
@@ -11,3 +9,8 @@ gene_up_id = getLDS(attributes = c("ensembl_gene_id"), filters = "ensembl_gene_i
                     martL = human, uniqueRows=T)
 `https://www.jianshu.com/p/33e22a558457`
 ### uniqueRows记住变成F，按照原始顺序排的
+# listAttributes(mouse) 查看所有的可以用的symbol，看库中的基因symbol有多少
+# attributes是你要转的symbol,mart是你要转的是什么物种的，martL是你转成什么物种的。
+# 常用的symbol列出：
+# ensembl_gene_id,entrezgene_id(gene_id),hgnc_symbol(人的gene symbol),mgi_symbol(老鼠的基因)
+(
